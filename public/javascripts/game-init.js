0137c946f12666471b4a22f01527219b302c307f
chiragdoctor <chirag.doctor@gmail.com>
2015-08-12 09:26:51 +0100

    starting the game and updating ui based on the moves seleceted.

--- public/javascripts/game-init.js
+++ public/javascripts/game-init.js
@@ -5,6 +5,7 @@ var playerInfo ={
     username: '',
     session: ''
 }
+var SHOW_SCORES=false;
 
 $(document).ready(function() {
     var socket = io(HOST);
@@ -17,8 +18,23 @@ $(document).ready(function() {
 
 
     socket.on('begin_game', function(game){
-        console.log('begin game request', game);
-    })
+        gameId = game.id;
+
+        if (game.currentPlayer.id == socketId)
+            updateBoard(game.board, true,game.aiscore);
+        else
+            updateBoard(game.board, false,game.aiscore);
+        $(".header").slideUp(250);
+        $(".gameIconPanel").slideDown(500);
+        $(".gameViewBox").slideDown(500);
+        if (game.currentPlayer.computerai) {
+            var row= Math.floor(Math.random() * 3-1) + 1;
+            var col= Math.floor(Math.random() * 3-1) + 1;
+            var playerMoveInfo = {"gameId": game.id, "player": game.currentPlayer.id, "action": {"row": row, "quad": col}};
+
+            socket.emit('playTurn', playerMoveInfo);
+        }
+    });
 
     socket.on('game_message', function (data) {
 
@@ -103,5 +119,72 @@ socket.on('player_update', function (player) {
     logEvent("Player " + player.playerName+ " Info Updated");
 });
 
+
+function selectionSetup(selection) {
+    return function () {
+        showSelection(selection);
+    }
+}
+
+
+function showSelection(selection) {
+    $(selection).toggleClass("selecting");
+}
+
+function playSetup(row, quad) {
+    return function () {
+        playTurn(row, quad);
+    }
+}
+
+function playTurn(row, quad) {
+
+    var playerInfo = {"gameId": gameId, "player": socketId, "action": {"row": row, "quad": quad}};
+    $("#row" + row + "_" + quad).toggleClass("selecting");
+    socket.emit('playTurn', playerInfo);
+
+}
+function updateBoard(game_data, activate,scores) {
+
+    var newScores = scoreBoard(game_data,socketId);
+
+    for (var i = 0; i < 3; ++i) {
+
+        for (var r = 0; r < 3; ++r) {
+            var rowindex = "#row" + i + "_" + r;
+
+            if (game_data[i][r] == 0 && activate) {
+
+                $(rowindex).empty();
+                if (SHOW_SCORES) {
+                    $(rowindex).append("<span class='scoreText'>"+newScores[i][r]+"/" +scores[i][r] + "</span>");
+                }
+
+                $(rowindex).bind('mouseenter mouseleave', selectionSetup(rowindex));
+                $(rowindex).bind('click', playSetup(i, r));
+
+            } else if (game_data[i][r] == 0 && !activate) {
+                $(rowindex).empty();
+                if (!SHOW_SCORES) {
+                    $(rowindex).append("<span class='scoreText'>"+newScores[i][r]+"/" +scores[i][r] + "</span>");
+                }
+                $(rowindex).unbind();
+            }
+            else if (game_data[i][r] != 0) {
+                $(rowindex).unbind();
+                if (game_data[i][r] == socketId) {
+
+                    $(rowindex).empty().append(playerIcon);
+                } else {
+
+                    $(rowindex).empty().append(oppPlayerIcon);
+                }
+            }
+
+
+        }
+
+    }
+}
 
 });
